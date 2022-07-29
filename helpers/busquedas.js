19bc592057d37edac8113531344b82af339d8759
Jhoandris B <andybaronp@gmail.com>
2022-07-28 22:13:55 -0500

    add intance Axios - api geoCoding

--- helpers/busquedas.js
+++ helpers/busquedas.js
@@ -10,14 +10,29 @@ export class Bsuquedas {
         //TODO ller de Db
     }
 
+    get paramsMaptiler() {
+        return {
+
+            'key': 'fzHjYb2p1bTZzey9h06D',
+            'language': 'es'
+        }
+    }
+
     async ciudad(lugar) {
 
         try {
 
+            const intanceAxios = axios.create({
+                baseURL: `https://api.maptiler.com/geocoding/[${lugar}].json `,
+                params: this.paramsMaptiler
+            })
+
             //peticion Http
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
+            const resp = await intanceAxios.get()
             console.log(resp.data)
             return []
+
+
         } catch (error) {
             return []
         }
